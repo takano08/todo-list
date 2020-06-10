1500b09770d04ee931954776669cec9cb09051ab
takano08 <65323032+takano08@users.noreply.github.com>
2020-06-10 17:56:14 +0900

    views-commit

--- app.js
+++ app.js
@@ -1,31 +1,14 @@
 const express = require('express');
-const mysql = require('mysql');
-
 const app = express();
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'list_app'
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.log('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('success');
-});
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  connection.query(
-    'SELECT * FROM users',
-    (error, results) => {
-      console.log(results);
-      res.render('todo.ejs');
-    }
-  );
+  res.render('todo.ejs');
+});
+
+app.get('/index', (req, res) => {
+  res.render('index.ejs');
 });
 
 app.listen(3000);
